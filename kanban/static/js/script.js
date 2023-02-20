b4051c34138625709116070a275b912144e80198
anthonyfdunphy <anthonyfdunphy@gmail.com>
2023-02-20 23:44:30 +0000

    kanban styling pretty much complete

--- kanban/static/js/script.js
+++ kanban/static/js/script.js
@@ -8,10 +8,12 @@ function toggleSidebar() {
     tab.addEventListener('click', function() {
       if (sidebar.classList.contains('open')) {
         console.log("The sidebar is open.");
-        document.documentElement.style.setProperty('--tab-width', '20%')
+        document.documentElement.style.setProperty('--tab-width', '20%');
+        document.documentElement.style.setProperty('--opacity-level','1');
       } else {
         console.log("The sidebar is closed.");
-        document.documentElement.style.setProperty('--tab-width', '0%')
+        document.documentElement.style.setProperty('--tab-width', '0%');
+        document.documentElement.style.setProperty('--opacity-level','0');
       }
     });
   }
