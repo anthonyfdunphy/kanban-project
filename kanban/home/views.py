c39ec058d830a3a397c72e1bccb1a50e73c09c5d
anthonyfdunphy <anthonyfdunphy@gmail.com>
2023-02-21 15:54:32 +0000

    added database connection or link between card details and creation and html home page

--- kanban/home/views.py
+++ kanban/home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from tasks.models import Tasks
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'home/index.html')
+    tasks = Tasks.objects.all()
+    cards = []
+    for task in tasks:
+        card = {
+            'title': task.title,
+            'content': task.content,
+        }
+        cards.append(card)
+    context = {'cards': cards}
+
+    return render(request, 'home/index.html', context)
