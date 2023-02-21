a84c5e1f0c3edd04f1ccd8a7c8d5bda0b1ae01fb
anthonyfdunphy <anthonyfdunphy@gmail.com>
2023-02-21 15:27:32 +0000

    created css structute and added model for task in admin

--- kanban/static/js/script.js
+++ kanban/static/js/script.js
@@ -1,3 +1,26 @@
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', function() {
+  console.log('Hamburger clicked');
+  hamburger.classList.toggle("is-active");
+
+  var sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('open');
+  let open = false;
+
+  if (sidebar.classList.contains('open')) {
+    console.log("The sidebar is open.");
+    document.documentElement.style.setProperty('--tab-width', '20%');
+    document.documentElement.style.setProperty('--opacity-level','1');
+  } else {
+    console.log("The sidebar is closed.");
+    document.documentElement.style.setProperty('--tab-width', '0%');
+    document.documentElement.style.setProperty('--opacity-level','0');
+  }
+
+});
+
+
 function toggleSidebar() {
     var sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('open');
