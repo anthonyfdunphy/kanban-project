a0c15a4f7a591608e4d8f00c4b3e1cad7c465428
anthonyfdunphy <anthonyfdunphy@gmail.com>
2023-02-22 00:04:18 +0000

    html and models up to date and working

--- kanban/home/views.py
+++ kanban/home/views.py
@@ -4,14 +4,18 @@ from tasks.models import Tasks
 # Create your views here.
 
 def index(request):
-    tasks = Tasks.objects.filter(status=0)
-    cards = []
-    for task in tasks:
-        card = {
-            'task': task.task,
-            'content': task.content,
-        }
-        cards.append(card)
-    context = {'cards': cards}
+    tasks_todo = Tasks.objects.filter(status=0)
+    tasks_inprog = Tasks.objects.filter(status=1)
+    tasks_done = Tasks.objects.filter(status=2)
+
+    todo_list = [{'task': task.task, 'content': task.content} for task in tasks_todo]
+    inprog_list = [{'task': task.task, 'content': task.content} for task in tasks_inprog]
+    done_list = [{'task': task.task, 'content': task.content} for task in tasks_done]
+
+    context = {
+        'todo_list': todo_list,
+        'inprog_list': inprog_list,
+        'done_list': done_list,
+    }
 
     return render(request, 'home/index.html', context)
--- kanban/tasks/models.py
+++ kanban/tasks/models.py
@@ -3,16 +3,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+# status_choices = [
+#         ('to-do', 'to-do'),
+#         ('in-progress', 'in-progres'),
+#         ('done', 'done'),
+# ]
+
+STATUS = (
+    (0,"To-Do"),
+    (1,"In-Progress"),
+    (2,"Complete")
+)
+
 class Tasks(models.Model):
     task = models.CharField(max_length=300, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    status_choices = [
-        ('to-do', 'to-do'),
-        ('in-progress', 'in-progres'),
-        ('done', 'done'),
-    ]
-    status = models.CharField(choices=status_choices, default='to-do', max_length=12)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
